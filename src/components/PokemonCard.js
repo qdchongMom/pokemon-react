ef37ac9b9a3db7088d376e6d34c5ee97917c3f93
qdchongMom <chong_qi_da@mom.gov.sg>
2021-08-10 16:10:44 +0800

    change

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./PokemonCard.css";
+
+function PokemonCard({ pokemon }) {
+  const { id, name, type, base } = pokemon;
+  return (
+    <div className="pokecard">
+      <img
+        className="img-pokemon"
+        src={`${process.env.PUBLIC_URL}/pokemonImage/1.png`}
+        alt="pokemon"
+      />
+      <div className="pokename">
+        <h1>{name.english}</h1>
+      </div>
+
+      <div className="type">
+        <span className="type1">{type[0]}</span>&nbsp;&nbsp;&nbsp;&nbsp;
+        <span>{type[1]}</span>
+      </div>
+      <div className="pokestats">
+        <div>HP: {base.HP}</div>
+        <div>Attack: {base.Attack}</div>
+        <div>Defence: {base.Defence}</div>
+        <div>SpAttack: {base.SpAttack}</div>
+        <div>SpDefence: {base.SpDefence}</div>
+        <div>Speed: {base.Speed}</div>
+      </div>
+    </div>
+  );
+}
+
+export default PokemonCard;
